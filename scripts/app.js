f37de6fad4915ebea645175203c02ff9ba403b4a
vrushika Ruwala <vrushikaroowala5@gmail.com>
2020-12-24 13:26:47 +0530

    commit

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+(function() {
+    const buttons = document.querySelectorAll('.counterBtn')
+    let count = 0
+
+
+    //Add event listeners and functionailty to each button  
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            if (button.classList.contains('prevBtn')) {
+                count--
+            } else if (button.classList.contains('nextBtn')) {
+                count++
+            }
+            else if(button.classList.contains('resetBtn')) {
+                count=0
+            }
+
+            //Select the counter text
+            const counter = document.querySelector('#counter')
+            counter.textContent = count
+
+            if (count < 0) {
+                counter.style.color = 'white'
+            } else if (count > 0) {
+                counter.style.color = '#153656'
+            } else {
+                counter.style.color = '#333333'
+            }
+        })
+    })
+})()
